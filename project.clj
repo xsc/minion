bf45abb96d19dff1a99b476793fa41e7380a3fb8
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-10-15 23:55:57 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [org.clojure/tools.nrepl "0.2.3"]]
+                 [org.clojure/tools.nrepl "0.2.6"]]
   :profiles {:dev {:dependencies [[com.taoensso/timbre "3.3.1"]]
                    :injections [(require 'clojure.tools.logging 'taoensso.timbre.tools.logging)
                                 (taoensso.timbre.tools.logging/use-timbre)
