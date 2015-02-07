8b37e43066a9a4bbf8db6d246a664fc5ea5ccf63
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-02-07 14:25:39 +0100

    add configurable nrepl options, bind to localhost by default.

--- src/minion/nrepl.clj
+++ src/minion/nrepl.clj
@@ -2,16 +2,26 @@
   (:require [clojure.tools.logging :refer [info debug]]
             [clojure.tools.nrepl.server :refer [start-server stop-server]]))
 
+(def ^:private default-options
+  {:bind "127.0.0.1"})
+
+(defn set-opts!
+  [nrepl-var opts]
+  (alter-meta! nrepl-var assoc :opts (into {} opts)))
+
 (defn start-nrepl!
   "Start nREPL and store in the given var."
   [nrepl-var port]
-  (alter-var-root nrepl-var
-                  #(do
-                     (when % (stop-server %))
-                     (info "starting up nREPL server on port" port "...")
-                     (let [s (start-server :port port)]
-                       (info "nREPL server is running ...")
-                       s))))
+  (let [opts (->> (-> nrepl-var meta :opts)
+                  (merge default-options)
+                  (apply concat))]
+    (alter-var-root nrepl-var
+                    #(do
+                       (when % (stop-server %))
+                       (info "starting up nREPL server on port" port "...")
+                       (let [s (apply start-server :port port opts)]
+                         (info "nREPL server is running ...")
+                         s)))))
 
 (defn stop-nrepl!
   "Stop nREPL stored in the given var."
