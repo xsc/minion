f2f985a3ab8f7b63ab22ed754dc5ff72c971bcef
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-02-15 13:23:35 +0100

    Version 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject minion "0.3.1-SNAPSHOT"
+(defproject minion "0.3.1"
   :description "Launcher Utilities"
   :url "https://github.com/xsc/minion"
   :license {:name "Eclipse Public License"
