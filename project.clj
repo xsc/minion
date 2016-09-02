15bb6d70ace70531e74ac4e055f6fe378cc1dd8c
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-09-02 10:29:10 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://github.com/xsc/minion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.nrepl "0.2.6"]]
-  :profiles {:base {:dependencies ^:replace []}
-             :dev {:dependencies [[com.taoensso/timbre "3.3.1"]]
+  :profiles {:dev {:dependencies [[com.taoensso/timbre "4.7.4"]]
                    :injections [(require 'clojure.tools.logging 'taoensso.timbre.tools.logging)
                                 (taoensso.timbre.tools.logging/use-timbre)
                                 (taoensso.timbre/set-level! :fatal)]}}
